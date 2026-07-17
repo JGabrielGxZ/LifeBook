cafc7ad8c57d84206410464c28051ac9fce76e90
JOSE GABRIEL CLAUDIO DE LIMA <20241134010011@ifrn.local>
2026-07-17 17:50:02 -0300

    mexendo um pouco no visual do index.php

--- index.php
+++ index.php
@@ -15,11 +15,20 @@ if (!isset($_SESSION['usuario_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="admin/css/css.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <title>LifeBook</title>
 </head>
 <body>
+    <p>
+        <h1 id="titulo1">Lifebook</h1>
+    </p>
+    <aside class="aside">
+        
+        Home
+        
+    </aside>
     <p >
-    <h1 style="color: white;">SEJA BEM VINDO <b><?php echo $_SESSION['usuario_nickname']; ?></b> AO LIFEBOOK!!</h1>
+    <!-- <h1 style="color: white;">SEJA BEM VINDO <b><?php echo $_SESSION['usuario_nickname']; ?></b> AO LIFEBOOK!!</h1> -->
     </p>
 </body>
 </html>
